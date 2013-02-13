4477ccdd7d896956934c9a4af861316d5a57176e
Luisa Hugerth <luisa.hugerth@scilifelab.se>
2013-02-13 16:25:07 +0100

    session4.py

--- hugerth/session4.py
+++ hugerth/session4.py
@@ -1,11 +1,11 @@
-import re, requests, datetime, operator, 
+import re, requests, datetime, operator
 from dateutil import parser
 from collections import defaultdict
 
-print "YO! This ain't working yet, yo!"
+print "ATTENTION: will fail if max API limit reached"
 
 # Define Weekday Dictionary
-weekdays = {'0': 'Monday', '1': 'Tuesday', '2': 'Wednesday', '3': 'Thursday', '4': 'Friday', '5': 'Saturday', '6': 'Sunday'}
+weekdays = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
 
 # Get clearance
 with open(".password") as secret:
@@ -21,53 +21,47 @@ commitcounts = []
 lencounts = []
 daycount = defaultdict(int)
 hourcount = defaultdict(int)
+emptycount = 0
 
 # For each of them, get all commits
 for repo in all_repos:
+	#repo = all_repos[0]
 	url = repo['commits_url']
 	url = url.split("{")[0]
 	commits = requests.get(url)
 	commits = commits.json()
-	for commit in commits:
-		partialcount+=1		#Count commits per user
-		message = commit['commit']['message']	# Do fun stuff with the messages
-												# This works in iPython, gives error when I try running!
-		words = message.split(' ')
-		length = len(words)	#Count number of words in commit messages
-		lencounts.append(length)
-		for word in words:			#Count word frequency in commit messages
-			wordcount[word]+=1
-		datime = parser.parse(commit['commit']['author']['date']) # Do fun things with the date
-		daycount[datime.day] += 1
-		hourcount[datime.hour] += 1
-	commitcounts.append(partialcount)
-	partialcount = 0
-
-## And it's at this point I'm told I have a syntax error. Any insights??
+	try:
+		for commit in commits:
+		#	commit = commits[0]
+			partialcount+=1		#Count commits per user
+			message = commit['commit']['message']	# Do fun stuff with the messages
+			words = message.split(' ')
+			length = len(words)	#Count number of words in commit messages
+			lencounts.append(length)
+			for word in words:			#Count word frequency in commit messages
+				wordcount[word]+=1
+			datime = parser.parse(commit['commit']['author']['date']) # Do fun things with the date
+			daycount[datime.weekday()] += 1	#Count frequency of commits per weekday
+			hourcount[datime.hour] += 1		#Count frequency of commits per full hour
+			commitcounts.append(partialcount)
+			partialcount = 0
+	except:
+		emptycount += 1		#Count number of empty repositories
 
 top_word = max(wordcount.iteritems(), key=operator.itemgetter(1))[0]
 top_wordcount = max(wordcount.iteritems(), key=operator.itemgetter(1))[1]
 top_day = max(daycount.iteritems(), key=operator.itemgetter(1))[0]
 top_day = weekdays[top_day]
 top_daycount = max(daycount.iteritems(), key=operator.itemgetter(1))[1]
-top_hour = max(daycount.iteritems(), key=operator.itemgetter(1))[0]
-top_hourcount = max(daycount.iteritems(), key=operator.itemgetter(1))[1]
+top_hour = max(hourcount.iteritems(), key=operator.itemgetter(1))[0]
+top_hourcount = max(hourcount.iteritems(), key=operator.itemgetter(1))[1]
 
 print "Most common day to commit: " + top_day + " - " + repr(top_daycount) + " commits"
 print "Most common hour to commit: " + repr(top_hour) + " - " + repr(top_hourcount) + " commits"
 print "Longest commit message: " + repr(max(lencounts)) + " words"
 print "Average commit message: " + repr(sum(lencounts)/len(lencounts)) + " words"
 print "Most common word in commit messages: " + top_word + " - " + repr(top_wordcount) + " times"
-print "Largest number of commits from a single user: " + repr(commitcount)
+print "Largest number of commits from a single user: " + repr(max(commitcounts))
 print "Average number of commits per user: " + repr(sum(commitcounts)/len(commitcounts))
-		
-
-
-## Give: most common day *ok
-## Most common hour of the day *ok 
-## Max number of commits per user *ok
-## Average number of commits per user *ok
-## Average length of commit message	*ok
-## Length of longest commit message *ok
-## Most common words in commit messages * ok
-
+if emptycount > 0:
+	print repr(emptycount) + " empty repositories were found"
