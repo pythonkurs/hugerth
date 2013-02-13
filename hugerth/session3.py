069fc3835f0516e72c0a05f21e3ca23aeb8578d4
Luisa Hugerth <luisa.hugerth@scilifelab.se>
2013-02-13 17:09:26 +0100

    Hope Guillermo is happy now

--- hugerth/session3.py
+++ hugerth/session3.py
@@ -4,15 +4,24 @@ import os.path
 
 class CourseRepo(object):
 	def __init__(self,lastname):
-		self.lastname = lastname
-		self.required=[".git", "setup.py", "README.md", 
-				"scripts/getting_data.py", "scripts/check_repo.py", 
-				"{lastname}/__init__.py".format(lastname=self.lastname), 
-				"{lastname}/session3.py".format(lastname=self.lastname)]
+		self.required = [".git", "setup.py", "README.md", 
+				"scripts/getting_data.py", "scripts/check_repo.py"]
+		self._also_required(lastname)
+		self._surname = lastname 
+
+	@property
+	def surname(self):
+		return self._surname
+
+	def _also_required(lastname):
+		self.required.append("{lastname}/__init__.py".format(lastname=lastname))
+		self.required.append("{lastname}/session2.py".format(lastname=lastname))
+		self.required.append("{lastname}/session3.py".format(lastname=lastname))
+
 	def check(self):
 		for file in self.required:
 			if not os.path.exists(file):
-				print file
+				print "Not found: " + file
 				return False
 		return True
 
