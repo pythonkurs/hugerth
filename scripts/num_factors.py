714f87362fc6d9fe0ac3c68fe1ae89fdd2c0e77a
Hugerth <luisa.hugerth@scilifelab.se>
2013-03-05 13:47:34 +0100

    Multiprocessor s7 is working

--- scripts/num_factors.py
+++ scripts/num_factors.py
@@ -1,3 +1,7 @@
+import sys
+import os
+import multiprocessing
+
 def factorize(n):
 	if n < 2:
 		return []
@@ -24,20 +28,61 @@ def dict_add (dic, element):
 	else:
 		dic[element] = 1
 
-lengths = dict()
+#def worker(tasks, results):
+#	t = tasks.get()
+#	result = factorize(t)
+#	results.put(result)
 
-#For every number from 2 to 500K:
-
-	factors = factorize()#number
-
+def task(args):
+	factors = factorize (args)
 	dic = dict()
-
 	for element in factors:
 		dict_add(dic, element)
+	return len(dic)
 
-	dict_add (lengths, len(dic))
+lengths = dict()
 
+if __name__ == '__main__':
 
+	if len (sys.argv) == 2:
+		n = 500000
+		mode = sys.argv[1]
+		print "Using standard value 500000\n"
+
+	elif len (sys.argv) == 3:
+		n = int(sys.argv[2])
+		mode = sys.argv[1]
+		
+	else:
+		print "Please use the format 'num_factors.py <mode> <number>',"
+		print "where mode is one of s(erial) m(ultiprocessing) or i(Python)."
+		print "Number is an integer larger than 2. If omitted, 500000 is used.\n"
+
+	n+=1
+
+	if mode == 's':
+		for number in range(2,n):
+			factors = factorize(number)
+			dic = dict()
+			for element in factors:
+				dict_add(dic, element)
+			dict_add (lengths, len(dic))
+	
+	elif mode == 'm':
+		cpus = multiprocessing.cpu_count()
+		if cpus > 4:
+			cpus = 4
+		pool = multiprocessing.Pool(processes=cpus)
+		result = pool.map(task, range(2,n))
+		for length in result:
+			dict_add(lengths, length)
+
+	elif mode == 'i':
+		pass
+
+	else:
+		print "Please select one of s(erial), m(ultiprocessing) or i(Python) modes\n"
+		
 
 print lengths
 
